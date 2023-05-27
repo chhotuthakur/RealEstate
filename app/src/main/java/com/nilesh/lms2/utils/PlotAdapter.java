bf6382a7112c2b1f4a8c7d1a7a46da03b5e57a5b
chhotuthakur <npsdahiya@gmail.com>
2023-05-27 22:14:27 +0530

    update 4.12

--- app/src/main/java/com/nilesh/lms2/utils/PlotAdapter.java
+++ app/src/main/java/com/nilesh/lms2/utils/PlotAdapter.java
@@ -1,4 +1,67 @@
 package com.nilesh.lms2.utils;
 
-public class PlotAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.nilesh.lms2.R;
+import com.nilesh.lms2.models.PlotModel;
+
+public class PlotAdapter extends FirebaseRecyclerAdapter<PlotModel,PlotAdapter.ViewHolder> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public PlotAdapter(@NonNull FirebaseRecyclerOptions<PlotModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull PlotAdapter.ViewHolder holder, int position, @NonNull PlotModel model) {
+
+//  TODO:      Glide.with().load(uploadCurrent.getmImageUrl()).into(holder.postImageView);
+        holder.title_txt.setText(model.getTitle());
+        holder.locate_txt.setText(model.getAd3());
+
+    }
+
+    @NonNull
+    @Override
+    public PlotAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view
+
+                = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyler_plot, parent, false);
+
+        return new PlotAdapter.ViewHolder(view);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        Context context;
+        ImageView plot_images;
+        TextView title_txt,locate_txt;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            plot_images = itemView.findViewById(R.id.plot_image);
+
+            title_txt = itemView.findViewById(R.id.title_txt_plot);
+            locate_txt =itemView.findViewById(R.id.location_txt_plot);
+
+
+
+
+        }
+    }
 }
